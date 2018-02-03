cc0468d703a5f4c04bee68fbe0490d77a2a1e7c8
paolozroxas <paolo.z.roxas@gmail.com>
2018-02-02 21:31:43 -0800

    started on searchHandler

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ angular.module('video-player')
   .component('app', {
     
     //don't use an arrow function here. It messes up the 'this' value.
-    controller: function ($window) {
+    controller: function ($window, $http) {
       this.videos = window.exampleVideoData;
       this.video = window.exampleVideoData[0];
       console.log(this.videos);
@@ -11,6 +11,24 @@ angular.module('video-player')
       this.clickHandler = (index) => {
         console.log(index, 'YEEE');
         this.video = this.videos[index];
+      };
+      //PLEASE send searchHandler down to search and listen for submit
+      this.searchHandler = (input) => {
+        console.log('input!');
+        var baseUrl = 'https://www.googleapis.com/youtube/v3';
+        var url = `${baseUrl}
+                        /search?part=snippet
+                        &q=${input}
+                        &type=video
+                        &maxResults=5
+                        &videoEmbeddable=true
+                        &key=${window.YOUTUBE_API_KEY}
+                      `;
+        $http.get(url)
+          .then((response) => {
+            console.log(response);
+            this.videos = response.results;
+          });
       };
     },
     
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 angular.module('video-player', [])
-.config(function($sceDelegateProvider) {
-  $sceDelegateProvider.resourceUrlWhitelist([
-    'self',
-    'https://www.youtube.com/**'
-  ]);
-});
+  .config(function($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+      'self',
+      'https://www.youtube.com/**'
+    ]);
+  });
+
+window.YOUTUBE_API_KEY = 'AIzaSyASXWJ5vnGbt6Xw4HeYaJhSagLRI3jyvXs';
